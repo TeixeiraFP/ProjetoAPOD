7e9afff5368c0b902ae1a977db1eea64db87ae60
topre <fpteixeira@outlook.com.br>
2021-10-28 20:46:23 -0300

    ajustes no css da img e infos da requisicao

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,34 @@
-$(`#btnsubmit`).click(function(){
+reqApi();
 
-    console.log(`to funcionando`)
+$(`#btnsubmit`).click(function (e) {
+  e.preventDefault();
+  console.log(`to funcionando`);
+  reqApi();
+});
 
-    $(`#data`).val()
+async function reqApi(data) {
+  const request = await $.ajax({
+    url:
+      `https://api.nasa.gov/planetary/apod?api_key=4Fv2LupBSUBdERhsn4Y4vQVjKUH2U5g7mcLuEAus&date=` +
+      $(`#data`).val(),
 
-    $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=4Fv2LupBSUBdERhsn4Y4vQVjKUH2U5g7mcLuEAus&date=` +  $(`#data`).val(),
-        
+    success: function (results) {
+      console.log(results);
 
-        success : function(results){
-            console.log(results)
+      $(`#tituloFoto`).html(`${results.title}`);
 
-            $(`#tituloFoto`).html(`${results.title}`)
-            
-            $(`#descricao`).html(`${results.explanation}`)
-            $(`#copy`).html(`${results.copyrigth}`)
+      $(`#descricao`).html(`${results.explanation}`);
+      $(`#copy`).html(`${results.copyrigth}`);
 
-            if(results.media_type === `image`){
-                $(`#imgDia`).html(`<img width = '380' heigth = '380'
-                
-                src = ${results.url}>`)
-            }
-            else if(results.media_type ===`video`){
-                $(`#imgDia`).html(`<iframe width = '480' heigth = '580' src = ${results.url}>`)
-            }
-        }
-    })
-})
+      if (results.media_type === `image`) {
+        $(`#imgDia`).html(`<img width = '380' heigth = '380'
+                  
+                  src = ${results.url}>`);
+      } else if (results.media_type === `video`) {
+        $(`#imgDia`).html(
+          `<iframe width = '480' heigth = '580' src = ${results.url}>`
+        );
+      }
+    },
+  });
+}
